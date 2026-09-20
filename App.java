50894046f0ebc40a7dbb5b7b4ceca3593b95e1a4
2022wa86060-gif <2022wa86060@wilp.bits-pilani.ac.in>
2026-09-20 13:33:17 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("BITS ID: 2022WA86060");
+    }
+}
